35783b1f4a82ca2ad16d903e8228ba74b2eed903
Marián <Matiasus@users.noreply.github.com>
2022-11-14 15:08:25 +0100

    Trim trailing spaces, add sources

--- lib/vs1053.h
+++ lib/vs1053.h
@@ -1,24 +1,28 @@
-/** 
- * ---------------------------------------------------------------+ 
+/**
+ * --------------------------------------------------------------------------------------+
  * @desc        VS1053 Driver (VLSI company)
- * ---------------------------------------------------------------+ 
+ * --------------------------------------------------------------------------------------+
  *              Copyright (C) 2022 Marian Hrinko.
  *              Written by Marian Hrinko (mato.hrinko@gmail.com)
  *
  * @author      Marian Hrinko
  * @datum       19.10.2022
- * @file        vs1053.h
- * @update      21.10.2022
+ * @file        vs1053.c
+ * @update      14.11.2022
  * @version     1.0
  * @tested      AVR Atmega328p
  *
- * @depend      
- * ---------------------------------------------------------------+
+ * @depend      spi.h, avr/io.h, util/delay.h
+ * --------------------------------------------------------------------------------------+
  * @interface   SPI connected through 7 pins
  * @pins        5V, DGND, MOSI, DREQ,  XCS
- *              5V, MISO,  SCK, XRST, XDCS 
+ *              5V, MISO,  SCK, XRST, XDCS
  *
- * @sources     https://www.vlsi.fi/fileadmin/app_notes/vs10XXan_spi.pdf
+ * @sources     https://www.vlsi.fi/fileadmin/datasheets/vs1053.pdf
+ *              https://www.vlsi.fi/fileadmin/app_notes/vs10XXan_spi.pdf
+ *              https://www.vlsi.fi/player_vs1011_1002_1003/modularplayer/files.html
+ *              https://github.com/adafruit/Adafruit_VS1053_Library
+ *              https://os.mbed.com/users/silis/code/VS1053//file/5ad25d480d5f/VS1053.cpp/
  */
 
 #ifndef __VS1053_H__
@@ -36,7 +40,7 @@
   #define VS1053_SCK          SPI_SCK
   #define VS1053_MOSI         SPI_MOSI
   #define VS1053_MISO         SPI_MISO
-  #define VS1053_XCS          SPI_SS       
+  #define VS1053_XCS          SPI_SS
   #define VS1053_DREQ         1
   #define VS1053_XDCS         0
 
@@ -46,18 +50,18 @@
   #define VS1053_XRST         7
 
   // REGISTERS
-  #define SCI_MODE            0x0  // Mode control  
-  #define SCI_STATUS          0x1  // Status of VS1053b 
-  #define SCI_BASS            0x2  // Built-in bass/treble control 
+  #define SCI_MODE            0x0  // Mode control
+  #define SCI_STATUS          0x1  // Status of VS1053b
+  #define SCI_BASS            0x2  // Built-in bass/treble control
   #define SCI_CLOCKF          0x3  // Clock freq + multiplier
   #define SCI_DECODE_TIME     0x4  // Decode time in seconds
-  #define SCI_AUDATA          0x5  // Misc. audio data 
-  #define SCI_WRAM            0x6  // RAM write/read  
+  #define SCI_AUDATA          0x5  // Misc. audio data
+  #define SCI_WRAM            0x6  // RAM write/read
   #define SCI_WRAMADDR        0x7  // Base address for RAM write/read
   #define SCI_HDAT0           0x8  // Stream header data 0
   #define SCI_HDAT1           0x9  // Stream header data 1
   #define SCI_AIADDR          0xA  // Start address of application
-  #define SCI_VOL             0xB  // Volume control  
+  #define SCI_VOL             0xB  // Volume control
   #define SCI_AICTRL0         0xC  // Application control register 0
   #define SCI_AICTRL1         0xD  // Application control register 1
   #define SCI_AICTRL2         0xE  // Application control register 2
@@ -67,7 +71,7 @@
   // SCI_MODE is used to control the operation of VS1053b and defaults to 0x4800 (SM_SDINEW set).
   #define SM_DIFF             0    // Differential [0 normal in-phase audio; 1 left channel inverted]
   #define SM_LAYER12          1    // Allow MPEG layers I & II [0 no; 1 yes]
-  #define SM_RESET            2    // Soft reset [0 no reset; 1 reset] 
+  #define SM_RESET            2    // Soft reset [0 no reset; 1 reset]
   #define SM_CANCEL           3    // Cancel decoding current file [0 no; 1 yes]
   #define SM_EARSPEAKER_LO    4    // EarSpeaker low setting [0 off; 1 active]
   #define SM_TESTS            5    // Allow SDI tests [0 allow; 1 not allow]
@@ -93,11 +97,11 @@
   #define SS_APDOWN1          2  //
   #define SS_AD_CLOCK         1  //
   #define SS_REFERENCE_SEL    0  //
-    
-  // READ / WRITE 
-  #define VS1053_READ         0x3 
+
+  // READ / WRITE
+  #define VS1053_READ         0x3
   #define VS1053_WRITE        0x2
-  
+
   // FUNCTION macros
   // bit is set?
   #define IS_BIT_SET(port, bit)             ( ((port) & (1 << (bit))) ? 1 : 0 )
@@ -107,14 +111,43 @@
   #define WAIT_IF_BIT_IS_SET(port, bit)     { while (IS_BIT_CLR(port, bit)); }
 
   /**
-   * @desc    DREQ Wait
+   * @desc    Write Serial Command Instruction
+   *
+   * @param   uint8_t addr
+   * @param   uint16_t command
+   *
+   * @return  void
+   */
+  void VS1053_WriteSci (uint8_t, uint16_t);
+
+  /**
+   * @desc    Read Serial Command Instruction
+   *
+   * @param   uint8_t addr
+   *
+   * @return  uint16_t
+   */
+  uint16_t VS1053_ReadSci (uint8_t);
+
+  /**
+   * @desc    Test SCI
    *
    * @param   void
    *
-   * @return  uint8_t
+   * @return  void
    */
-  //static inline uint8_t VS1053_DreqWait (void);
+  void VS1053_TestSci (void);
 
+  /**
+   * @desc    Write Serial Data
+   *
+   * @param   uint8_t * data
+   * @param   uint8_t bytes
+   *
+   * @return  int
+   */
+  int VS1053_WriteSdi (const uint8_t *, uint8_t);
+  
   /**
    * @desc    Hard reset
    * @source  https://www.vlsi.fi/player_vs1011_1002_1003/modularplayer/vs10xx_8c.html#a3
@@ -133,55 +166,8 @@
    *
    * @return  void
    */
-  void VS1053_SoftReset (void);
+  void VS1053_SoftReset (void);  
 
-  /**
-   * @desc    Test SDI - sine test
-   *
-   * @param   void
-   *
-   * @return  void
-   */  
-  void VS1053_SineTest (void);
-
-  /**
-   * @desc    Test SCI
-   *
-   * @param   void
-   *
-   * @return  void
-   */  
-  void VS1053_TestSci (void);
-
-  /**
-   * @desc    Write Serial Command Instruction
-   *
-   * @param   uint8_t addr
-   * @param   uint16_t command
-   *
-   * @return  void
-   */
-  void VS1053_WriteSci (uint8_t, uint16_t);
-  
-  /**
-   * @desc    Read Serial Command Instruction
-   *
-   * @param   uint8_t addr
-   *
-   * @return  uint16_t
-   */
-  uint16_t VS1053_ReadSci (uint8_t);
-  
-  /**
-   * @desc    Write Serial Data
-   *
-   * @param   uint8_t * data
-   * @param   uint8_t bytes
-   *
-   * @return  int
-   */  
-  int VS1053_WriteSdi (const uint8_t *, uint8_t);
-  
   /**
    * @desc    Init
    *
@@ -200,5 +186,14 @@
    * @return  void
    */
   void VS1053_SetVolume (uint8_t, uint8_t);
+  
+  /**
+   * @desc    Test SDI - sine test
+   *
+   * @param   void
+   *
+   * @return  void
+   */
+  void VS1053_SineTest (void);  
 
 #endif
