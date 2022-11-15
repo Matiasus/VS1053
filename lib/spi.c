a8354a600b519566397bc8c1f441c17e91f07c58
Marián <Matiasus@users.noreply.github.com>
2022-11-15 13:53:40 +0100

    Code form update

--- lib/spi.c
+++ lib/spi.c
@@ -1,23 +1,23 @@
-/** 
- * ---------------------------------------------------------------+ 
+/**
+ * ---------------------------------------------------------------+
  * @desc        SPI (Master Mode)
- * ---------------------------------------------------------------+ 
+ * ---------------------------------------------------------------+
  *              Copyright (C) 2022 Marian Hrinko.
  *              Written by Marian Hrinko (mato.hrinko@gmail.com)
  *
  * @author      Marian Hrinko
  * @datum       21.10.2022
  * @file        spi.c
- * @update      09.11.2022
+ * @update      15.11.2022
  * @version     1.0
  * @tested      AVR Atmega328p
  *
- * @depend      
+ * @depend
  * ---------------------------------------------------------------+
  * @interface   SPI master mode
- * @pins        SCLK, MOSI, MISO, CS (SS) 
+ * @pins        SCLK, MOSI, MISO, CS (SS)
  *
- * @sources    
+ * @sources
  */
 
 /* INCLUDE libraries */
@@ -32,11 +32,18 @@
  */
 void SPI_PortInit (void)
 {
-  SET_BIT (SPI_DDR, SPI_SS);       // as OUTPUT
-  SET_BIT (SPI_DDR, SPI_SCK);      // as OUTPUT
-  SET_BIT (SPI_DDR, SPI_MOSI);     // as OUTPUT
-  CLR_BIT (SPI_DDR, SPI_MISO);     // as INTPUT
-  SET_BIT (SPI_PORT, SPI_MISO);    // pullup activate
+  SPI_DDR |= (1 << SPI_SS)  |               // outputs
+             (1 << SPI_SCK) |               //
+             (1 << SPI_MOSI);               //
+  SPI_DDR &= ~(1 << SPI_MISO);              // inputs
+  SPI_PORT |= (1 << SPI_MISO);              // pullup activate
+/*
+  SET_BIT (SPI_DDR, SPI_SS);                // as OUTPUT
+  SET_BIT (SPI_DDR, SPI_SCK);               // as OUTPUT
+  SET_BIT (SPI_DDR, SPI_MOSI);              // as OUTPUT
+  CLR_BIT (SPI_DDR, SPI_MISO);              // as INTPUT
+  SET_BIT (SPI_PORT, SPI_MISO);             // pullup activate
+*/
 }
 
 /**
@@ -48,13 +55,16 @@ void SPI_PortInit (void)
  */
 void SPI_SlowSpeedInit (void)
 {
-  SPI_SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR1) | (1<<SPR0);
+  SPI_SPCR = (1 << SPE)  |                  // SPI Enable, note: writing a byte to the SPI data reg starts the SPI clock generator
+             (1 << MSTR) |                  // Master device
+             (1 << SPR1) |                  // Prescaler fclk/128 = 62500Hz
+             (1 << SPR0) ;                  //
 /*
   // NOTE: SPE must be set as last
-  SET_BIT (SPI_SPCR, MSTR);        // Master device
-  SET_BIT (SPI_SPCR, SPR0);        // 
-  SET_BIT (SPI_SPCR, SPR1);        // Prescaler fclk/128 = 62500Hz
-  SET_BIT (SPI_SPCR, SPE);         // SPI Enale, note: writing a byte to the SPI data reg starts the SPI clock generator
+  SET_BIT (SPI_SPCR, MSTR);
+  SET_BIT (SPI_SPCR, SPR0);                 //
+  SET_BIT (SPI_SPCR, SPR1);                 // Prescaler fclk/128 = 62500Hz
+  SET_BIT (SPI_SPCR, SPE);                  // SPI Enale, note: writing a byte to the SPI data reg starts the SPI clock generator
 */
 }
 
@@ -67,15 +77,17 @@ void SPI_SlowSpeedInit (void)
  */
 void SPI_FastSpeedInit (void)
 {
-  SPI_SPSR = (1<<SPI2X);
-  SPI_SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0);
+  SPI_SPCR = (1 << SPE)  |                  // SPI Enable, note: writing a byte to the SPI data reg starts the SPI clock generator
+             (1 << MSTR) |                  // Master device
+             (1 << SPR0) ;                  // f = fclk/16 = 0.5MHz
+  SPI_SPSR = (1 << SPI2X);                  // f*2
 /*
   // NOTE: SPE must be set as last
-  SET_BIT (SPI_SPCR, MSTR);        // Master device
-  SET_BIT (SPI_SPCR, SPR0);        // 
-  CLR_BIT (SPI_SPCR, SPR1);        // f = fclk/16 = 0.5MHz
-  SET_BIT (SPI_SPSR, SPI2X);       // f * 2 = 1MHz
-  SET_BIT (SPI_SPCR, SPE);         // SPI Enale, note: writing a byte to the SPI data reg starts the SPI clock generator
+  SET_BIT (SPI_SPCR, MSTR);                 // Master device
+  SET_BIT (SPI_SPCR, SPR0);                 //
+  CLR_BIT (SPI_SPCR, SPR1);                 // f = fclk/16 = 0.5MHz
+  SET_BIT (SPI_SPSR, SPI2X);                // f * 2 = 1MHz
+  SET_BIT (SPI_SPCR, SPE);                  // SPI Enale, note: writing a byte to the SPI data reg starts the SPI clock generator
 */
 }
 
@@ -85,13 +97,11 @@ void SPI_FastSpeedInit (void)
  * @param   uint8_t
  *
  * @return  void
- */   
+ */
 void SPI_WriteByte (uint8_t data)
 {
-  // start transmission
-  SPI_SPDR = data;
-  // wait for transmission complete
-  while(!(SPI_SPSR & (1<<SPIF)))
+  SPI_SPDR = data;                          // start transmission
+  while(!(SPI_SPSR & (1<<SPIF)))            // wait for transmission complete
   ;
 }
 
@@ -101,47 +111,39 @@ void SPI_WriteByte (uint8_t data)
  * @param   uint16_t
  *
  * @return  void
- */   
+ */
 void SPI_WriteWord (uint16_t data)
 {
-  // high byte
-  SPI_WriteByte ((uint8_t)(data >> 8));
-  // low byte
-  SPI_WriteByte ((uint8_t)(data & 0xFF));
+  SPI_WriteByte ((uint8_t)(data >> 8));     // high byte
+  SPI_WriteByte ((uint8_t)(data & 0xFF));   // low byte
 }
- 
+
 /**
  * @desc    SPI Read Byte
  *
  * @param   void
  *
  * @return  uint8_t
- */   
+ */
  uint8_t SPI_ReadByte (void)
  {
-  // start transmission, dummy byte
-  SPI_SPDR = 0xFF;
-  // wait for transmission complete
-  while(!(SPI_SPSR & (1<<SPIF)))
+  SPI_SPDR = 0xFF;                          // start transmission, dummy byte
+  while(!(SPI_SPSR & (1<<SPIF)))            // wait for transmission complete
   ;
-  // return received data
-  return SPI_SPDR;
+  return SPI_SPDR;                          // return received byte
  }
- 
+
 /**
  * @desc    SPI Write / Read Byte
  *
  * @param   uint8_t
  *
  * @return  uint8_t
- */   
+ */
  uint8_t SPI_WriteReadByte (uint8_t data)
  {
-  // start transmission
-  SPI_SPDR = data;
-  // wait for transmission complete
-  while(!(SPI_SPSR & (1<<SPIF)))
+  SPI_SPDR = data;                          // start transmission
+  while(!(SPI_SPSR & (1<<SPIF)))            // wait for transmission complete
   ;
-  // return received data
-  return SPI_SPDR;
+  return SPI_SPDR;                          // return received byte
  }
